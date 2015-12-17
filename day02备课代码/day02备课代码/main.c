3e2e72958f07e573d1160d6bf0d5fc3b3959a6f8
高明辉 <gaominghui@gaominghuideMacBook-Pro.local>
2015-12-17 16:52:57 +0800

    first commit

---
+++ day02备课代码/day02备课代码/main.c
@@ -0,0 +1,69 @@
+//
+//  main.c
+//  day02备课代码
+//
+//  Created by 高明辉 on 15/12/17.
+//  Copyright © 2015年 itcast. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+////
+////  main.c
+////  firstApp
+////
+////  Created by 高明辉 on 15/12/15.
+////  Copyright © 2015年 itcast. All rights reserved.
+////
+//
+//#include <stdio.h>
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    printf("Hello, World!\n");
+//    
+//    int num1,num2;
+//    char ch;
+//    scanf("%d",&num1);
+//    scanf("%c",&ch);
+//    scanf("%d",&num2);
+//    // 连续输入多个变量的值
+//    printf("num1 = %d,num2 = %d\n",num1,num2);
+//    printf("ch = %d\n",ch);
+//    printf("\\n = %d\n",'\n');
+//    printf(" 空格 = %d\n",' ');
+//    int a = 10;
+//    printf("%5d\n" , a);
+//    printf("%-5d\n",a);
+//    printf("%05d\n",a);
+//    
+//    float f1 = 10.134567823456f;// 不包含小数点在内的8位数字。
+//    printf("%6.3f\n",f1);
+//    
+//    double d1 = 1234567890123456.23456789876543;// 不包含小数点在内的17位数字。
+//    printf("%lf\n",d1);
+//    
+//    long l = 199999999;
+//    printf("%ld\n",l);
+//    
+//    int i = -1100;
+//    printf("%i\n",i );
+//    
+//    //    scanf("%i",&i);
+//    //    printf("%d\n",i);
+//    //
+//    //    scanf("%d",&i);
+//    //    printf("%d\n",i);
+//    
+//    int b = 0b101010;
+//    printf("%d\n",b);
+//    int o = 014237;
+//    printf("%d\n",o);
+//    
+//    return 0;
+//}
