95bf5ecd177a46d2b5200a85d3746aa5e0f967ab
高明辉 <gaominghui@gaominghuideMacBook-Pro.local>
2015-12-19 04:00:52 +0800

    homeworkday01

---
+++ day02备课代码/1-【掌握】常量和变量/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  1-【掌握】常量和变量
+//
+//  Created by 高明辉 on 15/12/17.
+//  Copyright © 2015年 itcast. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    printf("*\n");
+    printf("***\n");
+    printf("*****\n");
+    printf("*******\n");
+    
+    return 0;
+}
