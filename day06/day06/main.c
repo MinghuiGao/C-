4543074208e704d7a1a1cf5be4784e0c9c554be2
高明辉 <gaominghui@gaominghuideMacBook-Pro.local>
2016-01-02 22:21:01 +0800

    备课代码

---
+++ day06/day06/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  day06
+//
+//  Created by 高明辉 on 15/12/30.
+//  Copyright © 2015年 itcast. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
